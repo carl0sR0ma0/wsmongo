210801165b48670b4537af96340a280b2c003b18
carl0sR0ma0 <cvpromao@gmail.com>
2025-01-04 15:19:03 -0300

    :sparkles: Criando entity Post com user aninhado

---
+++ src/main/java/com/carlosromao/workshopmongo/repository/PostRepository.java
@@ -0,0 +1,11 @@
+package com.carlosromao.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.carlosromao.workshopmongo.domain.Post;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+
+}
